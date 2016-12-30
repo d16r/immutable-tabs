8744b9aca4a67b3532fbf71f9d2ac244ecf8dd39
Dustin Shahidehpour <dust@fb.com>
2016-12-30 11:25:14 -0500

    It's working

--- eventsPage.js
+++ eventsPage.js
@@ -1,7 +1,29 @@
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  chrome.tabs.get(activeInfo.tabId, function(tab) {
-    if (tab.pinned) {
-      alert("This tab is pinnned!");
-    }
+var tabs = {};
+
+chrome.tabs.query({ pinned: true }, function(results) {
+  results.forEach(function(tab) {
+    tabs[tab.id] = tab;
+  });
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.pinned) {
+    tabs[tabId] = tab;
+  } else {
+    delete tabs[tabId];
+  }
 });
+
+chrome.tabs.onRemoved.addListener(function(tabId) {
+  delete tabs[tabId];
+});
+
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+  var tab = tabs[details.tabId];
+  if (tab !== undefined && tab.pinned) {
+    return { redirectUrl: "javascript:" };
+  }
+}, {
+  urls: ["<all_urls>"],
+  types: ["main_frame"]
+}, ['blocking']);
